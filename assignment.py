2934213e270e97880ec2b513bfa64630f602367b
HalaKorhani <halakorhani2005@icloud.com>
2025-02-12 21:12:17 +0200

    added price on age based

---
+++ assignment.py
@@ -0,0 +1,10 @@
+# ex1
+age =int(input("enter your age "))
+if age<18:
+    print("the ticket price is 5$")
+elif age <=40:
+    print("the ticket price is 10$")
+else:
+    print("the ticket is 15$ ")
+
+
